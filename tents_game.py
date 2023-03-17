407d98e6e3039c7ab05b1e53c0e2b81053db67b3
Tafnes Silva Barbosa <tafnessilvabarbosa@gmail.com>
2023-03-17 16:37:23 -0300

    Autocreation of game and plot of game

--- tents_game.py
+++ tents_game.py
@@ -1,10 +1,15 @@
 import numpy as np
+import math
+import random
+import os
+from pdf2image import convert_from_path
+from PIL import Image
+import matplotlib.pyplot as plt
 
 # Constants
 TENT = 2
 TREE = 1
 EMPTY = 0
-OUTSIDE = -1
 
 class TentsGame(object):
     """
@@ -15,14 +20,39 @@ class TentsGame(object):
     tips_x and tips_y are the number of tents in each column and row respectively
     """
 
-    def __init__(self, size, trees_x, trees_y, tips_x, tips_y):
-        self.size = size
-        self.field = np.ones((size, size), dtype = int) * EMPTY
-        for x, y in zip(trees_x, trees_y):
-            self.field[y, x] = TREE
+    # def __init__(self, size, trees_x, trees_y, tips_x, tips_y):
+    #     self.size = size
+    #     self.field = np.ones((size, size), dtype = int) * EMPTY
+    #     for x, y in zip(trees_x, trees_y):
+    #         self.field[y, x] = TREE
 
-        self.tips_x = tips_x
-        self.tips_y = tips_y
+    #     self.tips_x = tips_x
+    #     self.tips_y = tips_y
+
+    def __init__(self, size=6, seed=None):
+        if seed != None:
+            random.seed(seed)
+
+        self.size = size
+        self.number_of_trees = int(math.floor(size * size / 5))
+        self.field = np.ones((size, size)) * EMPTY
+        done = False
+        while not done:
+            try:
+                self._construct_put_tents()
+                done = True
+            except:
+                done = False
+        (self.tips_x, self.tips_y) = self._get_tents_positions()
+        done = False
+        while not done:
+            try:
+                self._construct_put_trees()
+                done = True
+            except:
+                done = False
+        self._construct_remove_tents()
+        pass
 
     def __repr__(self):
         field_repr = '   '
@@ -44,6 +74,95 @@ class TentsGame(object):
                     field_repr += ' ' + '\U000025B2'
             field_repr += '\n'
         return field_repr
+    
+    def _construct_put_tents(self):
+        positions = []
+        for i in range(self.size):
+            for j in range(self.size):
+                positions.append((i, j))
 
-a = TentsGame(6, [1, 4, 3, 4, 0, 0, 1], [0, 0, 2, 2, 3, 4, 5], [2, 1, 1, 1, 1, 1], [2, 1, 0, 2, 0, 2])
-print(a)
+        tents = []
+        for k in range(self.number_of_trees):
+            tents.append(random.sample(positions, 1)[0])
+            positions.remove(tents[-1])
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if tents[-1][0] + i >= 0 and tents[-1][0] + i < self.size:
+                        if tents[-1][1] + j >= 0 and tents[-1][1] + j < self.size:
+                            if i != 0 or j != 0:
+                                try:
+                                    positions.remove((tents[-1][0]+i, tents[-1][1]+j))
+                                except:
+                                    pass
+
+        for tent in tents:
+            self.field[tent[0], tent[1]] = TENT
+        self.tents = tents
+
+    def _get_tents_positions(self):
+        tips_y = np.sum(self.field, axis=1) / TENT
+        tips_x = np.sum(self.field, axis=0) / TENT
+
+        return tips_x.astype(int).tolist(), tips_y.astype(int).tolist()
+
+    def _construct_put_trees(self):
+        possible_positions_to = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for tent in self.tents:
+            position_to = random.sample(possible_positions_to, 4)
+            for i in range(4):
+                if tent[0]+position_to[i][0] >= 0 and tent[0]+position_to[i][0] < self.size and tent[1]+position_to[i][1] >= 0 and tent[1]+position_to[i][1] < self.size:
+                    if self.field[tent[0]+position_to[i][0], tent[1]+position_to[i][1]] != TREE:
+                        self.field[tent[0]+position_to[i][0], tent[1]+position_to[i][1]] = TREE
+                        break
+                    else:
+                        continue
+    
+    def _construct_remove_tents(self):
+        for tent in self.tents:
+            self.field[tent[0], tent[1]] = EMPTY
+        self.tents = []
+
+    def show(self):
+        fileTEX = open('pic.tex', 'w', encoding='UTF-8')
+        fileTEX.write('\\documentclass{article}\n')
+        fileTEX.write('\\usepackage{fontspec}\n')
+        fileTEX.write('\\newfontfamily{\\NotoEmoji}{NotoColorEmoji.ttf}[Renderer=Harfbuzz]\n')
+        # fileTEX.write('\\newfontfamily{\\SymbolaEmoji}{Symbola}\n')
+        fileTEX.write('\\usepackage{tikz}\n')
+        fileTEX.write('\\usetikzlibrary{positioning,calc,3d,}\n')
+        fileTEX.write('\\usepackage{pgfplots}\n')
+        fileTEX.write('\\begin{document}\n')
+        fileTEX.write('\t\\resizebox{\\textwidth}{!}{\n')
+        fileTEX.write('\t\t\\begin{tikzpicture}\n')
+        # for i in range(self.size+1):
+        #     fileTEX.write(f'\t\t\t\\draw[thick] (0,{i})--++({self.size},0);\n')
+        #     fileTEX.write(f'\t\t\t\\draw[thick] ({i},0)--++(0,{self.size});\n')
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.field[i, j] == EMPTY:
+                    fileTEX.write(f'\t\t\t\\node[draw=black,thick,fill=brown!50, anchor=north west, minimum size = 1cm] (a{i}{j}) at ({j},{self.size-i})'+' {};\n')
+                elif self.field[i, j] == TREE:
+                    fileTEX.write(f"\t\t\t\\node[draw=black,thick,fill=green!50, anchor=north west, minimum size = 1cm] (a{i}{j}) at ({j},{self.size-i})"+' {\\NotoEmoji\\symbol{"1F333}};\n')
+                elif self.field[i, j] == TENT:
+                    fileTEX.write(f'\t\t\t\\node[draw=black,thick,fill=green!50, anchor=north west, minimum size = 1cm] (a{i}{j}) at ({j},{self.size-i})'+' {\\NotoEmoji\\symbol{"26FA}};\n')
+        # for i, x in enumerate(self.tips_x):
+        #     fileTEX.write(f'\t\t\t\\node[draw=black,thick,fill=brown!50, anchor=north west, minimum size = 1cm] (a{i}) at ({i},{self.size-i})'+' {'+f'{x}'+'};\n')
+        
+        fileTEX.write('\t\t\\end{tikzpicture}\n')
+        fileTEX.write('\t}\n')
+        fileTEX.write('\\end{document}')
+
+        fileTEX.close()
+
+        os.system('lualatex.exe -synctex=1 -interaction=nonstopmode "pic".tex > NUL 2>&1')
+
+        images = convert_from_path('pic.pdf')
+        for image in images:
+            image.save('pic.png', 'PNG')
+
+        image_original = np.asarray(Image.open('pic.png'))
+        plt.imshow(image_original[300:1300, 350:1450, :])
+        plt.grid(False)
+        plt.axis('off')
+        plt.show()
+        os.system('rm pic.*')
